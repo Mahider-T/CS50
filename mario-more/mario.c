aef7c499ee722545f478d6601bb56337b96dec4b
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-09 20:05:19 +0000

    Fri, Jun 9, 2023, 11:05 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-#include<cs50.h>
 
-int getNumber(void){
-    int levels;
-    do{
-        levels = get_int("How many levels? ");
-    }while(levels < 1 || levels > 8);
-    return levels;
+void pyramid(int height);
+
+int main() {
+    int number;
+
+    while (1) {
+        printf("Enter the length of the base of the pyramid: ");
+        if (scanf("%d", &number) != 1 || number < 1 || number >= 9) {
+            printf("Please input a positive numeric value between 1 and 8.\n");
+            while (getchar() != '\n') {}  // Clear input buffer
+        }
+        else {
+            break;
+        }
+    }
+
+    pyramid(number);
+
+    return 0;
 }
 
-void drawPyramid(int numberOfLevels){
-    for(int i = 1; i<= numberOfLevels; i++){
-        for(int j = numberOfLevels; j>= 1; j--){
-            if(i>=j){
-                printf("#");
-            }
-            else{
-                printf(" ");
-            }
+void pyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        for (int j = height; j > i; j--) {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            printf("#");
         }
         printf("  ");
-        for(int j = 1; j <= numberOfLevels; j++){
-            if(i>=j){
-                printf("#");
-            }
-            else{
-                printf(" ");
-            }
+        for (int k = 1; k <= i; k++) {
+            printf("#");
         }
         printf("\n");
     }
-
-}
-
-int main(void){
-    drawPyramid(getNumber());
-
 }

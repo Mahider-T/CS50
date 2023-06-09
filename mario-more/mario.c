e6e5f4eec7aa967fde72005ef1a0d0c488ba67fa
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-09 19:52:28 +0000

    Fri, Jun 9, 2023, 10:52 PM +03:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,39 @@
-#include <cs50.h>
 #include <stdio.h>
+#include<cs50.h>
 
-int main(void) 
-{
-    
+int getNumber(void){
+    int levels;
+    do{
+        levels = get_int("How many levels? ");
+    }while(levels < 1 || levels > 8);
+    return levels;
+}
+
+void drawPyramid(int numberOfLevels){
+    for(int i = 1; i<= numberOfLevels; i++){
+        for(int j = numberOfLevels; j>= 1; j--){
+            if(i>=j){
+                printf("#");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("  ");
+        for(int j = 1; j <= numberOfLevels; j++){
+            if(i>=j){
+                printf("#");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+int main(void){
+    drawPyramid(getNumber());
+
 }

e0b9eee5c9f12c1f15acdab4a862955424eb86b7
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-12 16:24:40 +0000

    Mon, Jun 12, 2023, 7:24 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,11 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("%i\n", text);
+    int numberOfLetters = count_letters(text);
+
+    printf("%i\n", numberOfLetters);
 }
 
 int count_letters(string text){

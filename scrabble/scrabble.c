52cb4aa4d5b59ec79ae4f78104e7ee06ba81b4a7
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-12 08:54:53 +0000

    Mon, Jun 12, 2023, 11:54 AM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,7 +19,15 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    
+    if(score1 > score2){
+        printf("Player 1 wins!");
+    }
+    else if(score2 > score1){
+        printf("Player 2 1ins!");
+    }
+    else{
+        printf("Tie!");
+    }
 
 }
 

29bfdc64aa79415c448a6f71aba4edb8044b5b94
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-14 14:04:42 +0000

    Wed, Jun 14, 2023, 5:04 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,6 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+void transformer(char arrayNormal[],char arrayKey[], string plainText);
+int positionOfCharacter(char array[], char targetCharacter);
+
 
 int main(int argc, string argv[])
 {
@@ -10,11 +13,12 @@ int main(int argc, string argv[])
 //Enter the key
 //Normal array of alphabet A = 1 and Z = 26
 //When the key is entered, put each character into a new array
-char transformer(char arrayNormal[],char arrayKey[], string plainText){
+void transformer(char arrayNormal[],char arrayKey[], string plainText){
     int lengthOfString = strlen(plainText);
     int positionOfChar;
     for(int i = 0; i < lengthOfString; i++){
-        positionOfChar = positionOfCharacter()
+        positionOfChar = positionOfCharacter(plainText[i]);
+        printf(arraykey[positionofChar]);
     }
 }
 

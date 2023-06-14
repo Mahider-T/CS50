5b7ed3c4cebfb8f513d6c2c56239a9944b8e8153
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-14 13:53:22 +0000

    Wed, Jun 14, 2023, 4:53 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -10,7 +10,9 @@ int main(int argc, string argv[])
 //Normal array of alphabet A = 1 and Z = 26
 //When the key is entered, put each character into a new array
 char transformer(char arrayNormal[],char arrayKey[], string plainText){
-    
+    for(int i = 0; i < 26; i++){
+        
+    }
 }
 
 int positionOfCharacter(char array[], char targetCharacter){

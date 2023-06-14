57e9d32432317ff2a69438987f4f80fb698d440d
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-14 13:37:06 +0000

    Wed, Jun 14, 2023, 4:37 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
 
 }
+
+//Enter the key
+//Normal array of alphabet A = 1 and Z = 26
+//

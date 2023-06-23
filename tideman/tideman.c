4a1c259c8431002c51d82265cc386241a63a5eef
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-23 14:46:12 +0000

    Fri, Jun 23, 2023, 5:46 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -123,7 +123,7 @@ void record_preferences(int ranks[])
     //because i by definition has better rank (lower index in ranks array) than j
     for (int i = 0; i < candidate_count-1; i++) {
         for( int j = i + 1; j < candidate_count; j++){
-            preferences[i][j] += 1;
+            preferences[ranks[i]][ranks[j]] += 1;
         }
     }
     return;

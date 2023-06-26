883b3122a8bd721e834325694f2249cfdef9faf7
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-26 09:14:27 +0000

    Mon, Jun 26, 2023, 2:14 AM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,11 +3,11 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    RGBTRIPLE pixel;
+    // RGBTRIPLE pixel;
     for ( int i = 0; i < height; i++){
         for (int j = 0; j < width; j++){
-            if(pixel.rgbtBlue == 0x00 && pixel.rgbtGreen == 0 && pixel.rgbtRed == 0){
-                pixel.rgbtBlue = 0xff;
+            if(image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0){
+                image[i][j].rgbtBlue = 0xff;
             }
         }
     }

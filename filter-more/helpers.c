de8be4cf4f5b94ad4b46b5bc9069e34bbac89d0d
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-26 11:34:57 +0000

    Mon, Jun 26, 2023, 4:34 AM -07:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -5,7 +5,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for ( int i = 0; i < height; i++){
         for ( int j = 0; j < width; j++){
-            image[i][j].rgbtBlue = 0xff;
+            char *red = image[i][j].rgbtRed;
+            char *green = image[i][j].rgbtGreen;
+            char *blue = image[i][j].rgbtBlue;
+
+            
+
         }
     }
     return;

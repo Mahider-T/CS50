5d546a673ca38aecc2d12cd754b52aa3264d3812
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-27 01:10:17 +0000

    Mon, Jun 26, 2023, 6:10 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,12 +8,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *photo = argv[1];
-    FILE *file = fopen(photo, "r");
+    // char *photo = argv[1];
+    FILE *file = fopen(argv[1], "r");
 
     if (file == NULL)
     {
-        printf("Could not open %s.\n", photo);
+        printf("Could not open %s.\n", argv[1]);
         return 1;
     }
 

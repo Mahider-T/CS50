c873d2b233f4ad0d0aebc53c34e7c4f67ccedeff
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-06-27 21:58:48 +0000

    Tue, Jun 27, 2023, 2:58 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,17 +21,20 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+
+        int count;
+        int started;
     while (fread(block, 1, 512, file) == 512)
     {
-        int count = 0;
-        int started = 0;
+        count = 0;
+        started  = 0;
         if(block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff /*&& (block[3] & 0xf0) == 0xe0*/){
             // sprintf(filename, "%03.i.jpg", count);
             // FILE *img = fopen("one.jpg", "w");
             // fwrite(&block, 512, 1, img);
             // count++;
-            // printf("%i",count);
             count++;
+            printf("%i",count);
         }
         // printf("%i\n",count);
     }

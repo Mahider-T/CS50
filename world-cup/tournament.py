db5bbb5ba88e30e8ccefa312c5a49c40e197fca7
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-06 14:51:54 +0000

    Thu, Jul 6, 2023, 5:51 PM +03:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,7 +17,7 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
     for i in teams:
-        print(teams[i][team])
+        print(teams[i][tea])
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts

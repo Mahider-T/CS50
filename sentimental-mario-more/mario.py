55debe5509146c3f1ab13a83567508135cb20e6c
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-07 08:23:01 +0000

    Fri, Jul 7, 2023, 11:23 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -8,7 +8,9 @@ def main():
 
     for i in range(height, 0, -1):
         for j in range(height):
-            if()
+            if(i >= j):
+                print("#", end = "")
+        print()
 
     for i in range(height, 0, -1):
         for j in range(height, 0, -1):

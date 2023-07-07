c7842c21848bae5642eeea23f7d23b2465418e9f
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-07 08:19:14 +0000

    Fri, Jul 7, 2023, 11:19 AM +03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,11 +2,18 @@
 from cs50 import get_int
 def main():
     height = get_int("Height: ")
+
     while(height < 1 or height > 8):
         height = get_int("Height: ")
-    for i in range(height):
-        for i in range(height):
-            
+        
+    for i in range(height, 0, -1):
+        for j in range(height, 0, -1):
+
+    for i in range(height, 0, -1):
+        for j in range(height, 0, -1):
+            if(i <= j):
+                print("#", end = "")
+        print()
 
 if __name__ == "__main__":
     main()

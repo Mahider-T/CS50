20e8732e091011c59b28f3e542156418134625fc
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-08 07:15:01 +0000

    Sat, Jul 8, 2023, 10:15 AM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -14,8 +14,15 @@ def isValidCreditCard(number):
     if(len(empty) != 13 or len(empty) != 15 or len(empty) != 16 ):
         return False
     sum = 0
+    count = 0
     for digit in range(len(empty), -1, -1):
-        if
+        if(count %2 == 0):
+            sum += empty[digit]
+        else:
+            sum += 2*empty[digit]
+
+    if(sum % 10):
+        
 
     # print(empty)
 

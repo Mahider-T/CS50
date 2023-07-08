0ed4747d778c5dee14557515bf8ee784787d1c1b
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-08 06:55:29 +0000

    Sat, Jul 8, 2023, 9:55 AM +03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,7 +1,13 @@
 # TODO
 from cs50 import get_int
-def getDigitSum():
+def getDigitSum(number):
+    sum = 0
+    while(number > 0):
+        sum += number % 10
+        number /= 10
+    return sum
 def isValidCreditCard():
+    
 def getCardType():
 
 def main():

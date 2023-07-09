13f818a877fb19ab776155a3779de9e5a7628e72
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-09 17:31:18 +0000

    Sun, Jul 9, 2023, 8:31 PM +03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,12 +1,23 @@
 # TODO
-def numberOfSentences(text):
+def count_sentences(text):
     count = 1
     for i in text:
         if i == '.' or i == '?' or i == '!':
             count += 1
     return count
+
+def count_words(text):
+    count = 1
+    for i in text:
+        if i == ' ':
+            count += 1
+    return count
+
+def count_letters(text):
+    for i in text:
+        
 def main():
-    print(numberOfSentences("What is your name? My nane is Mahider, How about you? bro!"))
+    print(count_words("What is"))
 
 if __name__ == "__main__":
     main()

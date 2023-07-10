851752cee843240f355e7e53cda95aa9d0a2f71c
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-10 18:49:46 +0000

    Mon, Jul 10, 2023, 9:49 PM +03:00

--- songs/1.sql
+++ songs/1.sql
@@ -1,17 +1,1 @@
--- CREATE TABLE songs (
---     id INTEGER,
---     name TEXT,
---     artist_id INTEGER,
---     danceability REAL,
---     energy REAL,
---     key INTEGER,
---     loudness REAL,
---     speechiness REAL,
---     valence REAL,
---     tempo REAL,
---     duration_ms INTEGER
--- );
--- CREATE TABLE artists (
---     id INTEGER,
---     name TEXT
--- );
+SELECT name FROM songs;

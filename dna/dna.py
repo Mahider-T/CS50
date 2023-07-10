82fd2c6db01b841c39309564f059e9c3fb8e97ae
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-10 10:43:53 +0000

    Mon, Jul 10, 2023, 1:43 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -36,23 +36,22 @@ def main():
     target = []
     for i in range(1,len(header)):
         target.append(longest_match(string,header[i]))
-    # print(target)
+    print(target)
 
     # TODO: Check database for matching profiles
-        for i in range(len(database)):
-            isMatch = 0
-            for j in range(1, len(header)):
-                if int(database[i][header[j]]) == target[j - 1]:
-                    isMatch = 1
-                else:
-                    isMatch = 0
-            # print(database[i][header[j]], end = " ")
-            # print(target[j - 1])
-            if isMatch == 1:
-                print(database[i]['name'])
-                sys.exit(0)
-        print("no match")
-
+    for i in range(len(database)):
+        isMatch = 0
+        for j in range(1, len(header)):
+            if int(database[i][header[j]]) == target[j - 1]:
+                isMatch = 1
+            else:
+                isMatch = 0
+        # print(database[i][header[j]], end = " ")
+        # print(target[j - 1])
+    if isMatch == 1:
+        print(database[i]['name'])
+        sys.exit(0)
+    print("no match")
     return
 
 

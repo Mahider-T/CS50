f5e1af2752c1f9724e7befc52bf367eef46ee2c8
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-11 09:02:38 +0000

    Tue, Jul 11, 2023, 12:02 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+--This finds the streets and id of crimes on which crimes have been commited on July 28, 2021
+SELECT id, street FROM crime_scene_reports WHERE year = 2021 AND month = 07 AND day = 28;
++-----+-----------------+
+--| id  |     street      |
+--+-----+-----------------+
+--| 293 | Axmark Road     |
+--| 294 | Boyce Avenue    |
+--| 295 | Humphrey Street |
+--| 296 | Widenius Street |
+--| 297 | Humphrey Street

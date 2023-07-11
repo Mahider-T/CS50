8ed54f4bf88b5281c5475870725e41195250c61e
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-11 07:35:00 +0000

    Tue, Jul 11, 2023, 10:35 AM +03:00

---
+++ movies/12.sql
@@ -0,0 +1,1 @@
+SELECT movies.title FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND people.name = "Adam Sandler" AND people.name = "Jennifer Lawrence";

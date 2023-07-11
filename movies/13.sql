383bd5429ab2a12268048b62f74879995fe4cff6
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-11 08:45:21 +0000

    Tue, Jul 11, 2023, 11:45 AM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,1 +1,1 @@
-SELECT people.name FROM people, stars, movies WHERE people.id = stars.person_id AND movies.id = stars.movie_id AND movies.id IN (SELECT movies.id FROM movies, stars, people WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND people.name = "Kevin Bacon" AND birth = 1958) AND people.name != "Kevin Bacon";
+SELECT people.name FROM people, stars, movies WHERE people.id = stars.person_id AND movies.id = stars.movie_id AND movies.id IN (SELECT movies.id FROM movies

f0378ed2f23597bc7d4830edfbf44a1df481ee2c
Mahider  Tekola Ayfokiru <95926290+Mahider-T@users.noreply.github.com>
2023-07-12 09:20:49 +0000

    Wed, Jul 12, 2023, 12:20 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -30,14 +30,7 @@ airports              crime_scene_reports   people
 atm_transactions      flights               phone_calls
 bakery_security_logs  interviews
 bank_accounts         passengers
-CREATE TABLE people (
-    id INTEGER,
-    name TEXT,
-    phone_number TEXT,
-    passport_number INTEGER,
-    license_plate TEXT,
-    PRIMARY KEY(id)
-);
+
 CREATE TABLE phone_calls (
     id INTEGER,
     caller TEXT,
@@ -49,6 +42,43 @@ CREATE TABLE phone_calls (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE people (
+    id INTEGER,
+    name TEXT,
+    phone_number TEXT,
+    passport_number INTEGER,
+    license_plate TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE flights (
+    id INTEGER,
+    origin_airport_id INTEGER,
+    destination_airport_id INTEGER,
+    year INTEGER,
+    month INTEGER,
+    day INTEGER,
+    hour INTEGER,
+    minute INTEGER,
+    PRIMARY KEY(id),
+    FOREIGN KEY(origin_airport_id) REFERENCES airports(id),
+    FOREIGN KEY(destination_airport_id) REFERENCES airports(id)
+);
+
+CREATE TABLE passengers (
+    flight_id INTEGER,
+    passport_number INTEGER,
+    seat TEXT,
+    FOREIGN KEY(flight_id) REFERENCES flights(id)
+);
+CREATE TABLE airports (
+    id INTEGER,
+    abbreviation TEXT,
+    full_name TEXT,
+    city TEXT,
+    PRIMARY KEY(id)
+);
+
 --suspects based on license plate
 SELECT people.name FROM people, bakery_security_logs WHERE people.license_plate = bakery_security_logs.license_plate AND year = 2021 AND month = 07 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <=25 AND activity = "exit";
 --Barry, Bruce, Diana, Iman, Kelsey, Luca, Sofia, Vanessa
@@ -57,6 +87,15 @@ SELECT people.name FROM people, bakery_security_logs WHERE people.license_plate 
 SELECT people.name FROM people, phone_calls WHERE people.phone_number = phone_calls.caller AND year = 2021 AND month = 07 AND day = 28 AND duration <= 60;
 --Benista, Bruce, Carina, Diana, Kathryn,Kelsey, Kenny, Sofia, Taylor
 
---common = {Bruce: 233, Diana: 255, Kelsey: 224 & 251, Sofia  }
+--common suspects: phone_call id = {Bruce: 233, Diana: 255, Kelsey: 224 & 251, Sofia: 221 }
 
+--possiible accomplices
+SELECT people.name FROM people, phone_calls WHERE people.phone_number = phone_calls.receiver AND year = 2021 AND month = 07 AND day = 28 AND duration <= 60 AND (phone_calls.id = 233 OR phone_calls.id = 255 OR phone_calls.id = 224 OR phone_calls.id = 251 OR phone_calls.id = 221);
+--accomplices: phone_call_id = { Jack: 221, Larry:224, Robin: 233, Melissa: 251, Philip: 255}
 
+--The possible duos (thief:accomplice) = {Bruce: Robin, Diana: Philip, Kelsey: Larry, Kelsey: Melissa, Sofia: Jack}
+
+SELECT people.name, flights.id, passengers.passport_number, flights.hour, flights.minute FROM people, flights, passengers WHERE people.passport_number = passengers.passport_number AND flights.id = passengers.flight_id AND year = 2021 AND month = 07 AND day = 29 ORDER BY hour ASC, minute ASC;
+--peope who took first flight on 29 july 2021 = {Doris, Sofia, Bruce, Edward, Kelsey, Taylor, Kenny, Luca}
+
+--filtered duos = {}
